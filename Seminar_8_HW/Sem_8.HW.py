f718a72b4fe06d4226b9528eba67d6d46f59931e
L0GIN0UT <ivan.login.02@mail.ru>
2023-08-08 22:54:35 +0300

    добавил защиту от дурака

--- Seminar_8_HW/Sem_8.HW.py
+++ Seminar_8_HW/Sem_8.HW.py
@@ -5,11 +5,22 @@ import re
 # ВНИМАНИЕ ИЗМЕНИТЕ ПУТЬ В filepath ДЛЯ КОРРЕКТОЙ РАБОТЫ ПРОГРАММЫ
 filepath = os.path.join("c:/Users/Ivanlogin888/Desktop/Python/Python_semiar_lectures/Seminar_8_HW",'Users_data.txt')
 
+# Функция для проверки длины сообщения пользователя
+def checking_for_input_length(arr):
+    if len(arr) > 4:
+        arr = arr[:4]
+    if len(arr) < 4:
+        a = len(arr)
+        while a < 4:
+            arr.append('None')
+            a += 1
+    return arr
+
 # Функция для выбора функций
 def choose():
     while True:
         choise = input('Введите действие (1,2,3,4,5) -> ').lower()
-        if choise == '1': return zapis(input('Введите Фамилию Имя Отчество и Номер телефона человека через пробел -> ').split())
+        if choise == '1': return zapis(checking_for_input_length(input('Введите Фамилию Имя Отчество и Номер телефона человека через пробел -> ').split()))
         if choise == '2': return read_search(input('Введите Фамилию человека которого вы ищите -> '))
         if choise == '3': return delete(input('Введите Фамилию человека для удаления его данных -> '))
         if choise == '4': return change(input('Введите Фамилию человека для замены его данных -> '), input('Введите что вы хотите изменить (1 - Фамилия, 2 - Имя, 3 - Отчество, 4 - Номер) -> ') , input('Введите новые данные -> '))
